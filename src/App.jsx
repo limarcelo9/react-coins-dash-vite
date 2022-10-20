64af06db0c79319532bff731d0e5ca6a93cdc18a
Marcelo Lima <limarcelo90@gmail.com>
2022-10-19 22:08:27 -0300

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import  styles from './App.module.css'
+import './global.css';
+
+function App() {
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Hello First React Vite</h1>
+    </div>
+  )
+}
+
+export default App
